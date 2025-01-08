abe2c9b4f8a85c6d3ce74fc365b5c6065c354871
Thomas Alexander <42557034+ttalexander2@users.noreply.github.com>
2025-01-08 16:02:31 -0700

    Updated example

--- example/main.cpp
+++ example/main.cpp
@@ -6,6 +6,8 @@
 
 #include <fmt/format.h>
 
+#include "arm7tdmi/memory.h"
+
 int main() {
 
     u32 branch_and_exchange = 0x112fff14;
@@ -20,23 +22,15 @@ int main() {
     const auto multiply_long_instr = arm7tdmi::arm::decode(multiply_long);
     fmt::println("{:#034b} - {}", multiply_long, instruction_to_string(multiply_long_instr));
 
-    const auto cpu = arm7tdmi::cpu::create<arm7tdmi::allocator>(0xffffffff / sizeof(u32));
-    if (!cpu) {
-        fmt::println("Error constructing cpu.");
-        return 0;
-    }
+    auto memory = arm7tdmi::basic_memory(256);
+    auto cpu = arm7tdmi::cpu(&memory);
 
-    cpu->registers.cpsr = 32u;
-    fmt::println("{:#034b}", cpu->registers.cpsr);
+    cpu.registers.cpsr(32u);
+    fmt::println("{:#034b}", cpu.registers.cpsr());
 
-    cpu->registers.cpsr.set_c(true);
-    bool c = cpu->registers.cpsr.get_c();
-    cpu->registers.pc++;
-
-    fmt::println("{:#034b}", cpu->registers.cpsr);
-    fmt::println("cpu size: {}, data size: {}, register size: {}", sizeof(arm7tdmi::cpu) / sizeof(u32), 19,  sizeof(arm7tdmi::cpu_register) / sizeof(u32));
-
-    fmt::println("Endianness: {}", arm7tdmi::util::is_big_endian() ? "big" : "little");
+    cpu.registers.cpsr_set_c(true);
+    bool c = cpu.registers.cpsr_get_c();
+    cpu.registers.pc(cpu.registers.pc() + 1);
 
     return 0;
 }
